9313473fba1c2f2ead02fe86f02525da58b43c73
John <johnathanfuller0@gmail.com>
2026-05-08 10:56:43 -0400

    center final project card

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
+<<<<<<< HEAD
 test('renders John Fuller heading', () => {
+=======
+test("renders learn react link", () => {
+>>>>>>> 48bdbf4 (center final project card)
   render(<App />);
   const heading = screen.getByText(/john fuller/i);
   expect(heading).toBeInTheDocument();
